c6a40d4e429e6b1335f6a3b6beb38fe1c6ebaa7a
sib-vishal <vishal.maurya@sibinfo.tech>
2025-05-02 18:25:21 +0530

    commit initial

---
+++ include/footer.php
@@ -0,0 +1,79 @@
+<footer class="footer py-5">
+	<div class="containerFull">
+		<div class="row">
+			<div class="col-lg-4">
+				<div class="footer-logo">
+					<img src="images/logo.svg" alt="" class="img-fluid" />
+
+				</div>
+
+				<h4 class="heading text-white">
+					<span>RNB Global University</span><br />
+					Development Office
+				</h4>
+
+			</div>
+			<div class="footer-menu col-lg-8">
+				<div class="row row-cols-3">
+					<ul>
+						<li><a href="#">Home</a></li>
+						<li><a href="#">About</a></li>
+						<li><a href="#">Rural Excellence</a></li>
+						<li><a href="#">Impact Stories</a></li>
+					</ul>
+
+					<ul>
+						<li><a href="#">Ambition</a></li>
+						<li><a href="#">Donate</a></li>
+						<li><a href="#">Contact</a></li>
+						<li><a href="#">RNB Global University</a></li>
+					</ul>
+
+					<ul>
+						<li><strong class="social_title">Follow us</strong>
+
+							<ul class="social-icon p-0 mt-3  d-flex gap-1 flex-wrap border-0">
+								<li>
+									<a href="#">
+										<i class="fa-brands fa-x-twitter"></i>
+									</a>
+								</li>
+								<li>
+									<a href="#">
+										<i class="fa-brands fa-linkedin-in"></i>
+									</a>
+								</li>
+								<li>
+									<a href="#">
+										<i class="fa-brands fa-youtube"></i>
+									</a>
+								</li>
+							</ul>
+						</li>
+
+					</ul>
+				</div>
+			</div>
+
+
+
+		</div>
+
+	</div>
+
+</footer>
+<div class="copy-right-main py-3">
+
+	<div class="containerFull">
+		<div class="copyRight d-flex gap-5 flex-wrap align-items-center justify-content-center ">
+			<p class="textFooter">© <?php echo date("Y"); ?> RNB University. All rights reserved.
+
+			</p>
+			<ul class="d-flex ">
+				<li><a href="#">Privacy Policy</a></li> |
+				<li><a href="#">Terms & Conditions</a></li> |
+				<li><a href="#">Disclaimer</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
---
+++ index.php
@@ -0,0 +1,91 @@
+<?php $page = 'home'; ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Alfa Education </title>
+    <?php include 'include/head-links.php'; ?>
+    <meta property="og:url" content="<?php echo $page_url ?>">
+    <meta property="og:title" content="">
+    <meta property="og:description" content="">
+    <meta property="og:image" content="images/logo.png">
+    <meta name="twitter:card" content="">
+    <meta name="twitter:site" content="<?php echo $page_url ?>">
+    <meta name="twitter:title" content="">
+    <meta name="twitter:description" content="">
+    <meta name="twitter:image" content="images/logo.png">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+
+</head>
+
+<body onload="createCaptcha();">
+    <div class="wrapper ">
+        <?php include 'include/header.php'; ?>
+
+        <div class="banner-wrapper ">
+            <div class="containerFull">
+                <img src="images/banner/home-banner.svg" alt="" class="img-fluid w-100" />
+
+            </div>
+
+        </div>
+
+        <?php include 'include/footer.php'; ?>
+
+    </div>
+    <?php include 'include/footer-links.php'; ?>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script>
+        var swiper = new Swiper(".awards_slider", {
+            spaceBetween: 20,
+            centeredSlides: true,
+            slidesPerView: 1.5,
+            loop: true,
+            autoplay: {
+                delay: 5000,
+                disableOnInteraction: false,
+            },
+
+            // breakpoints: {
+
+            //     320: {
+            //         slidesPerView: 1,
+            //         spaceBetween: 10
+            //     },
+
+            //     480: {
+            //         slidesPerView: 1,
+            //         spaceBetween: 20
+            //     },
+
+            //     640: {
+            //         slidesPerView: 2,
+            //         spaceBetween: 30
+            //     },
+
+            //     768: {
+            //         slidesPerView: 2,
+            //         spaceBetween: 30
+            //     }
+            //     1024: {
+            //         slidesPerView: 3,
+            //         spaceBetween: 30
+            //     }
+            // }
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+
+            navigation: {
+                nextEl: ".next_1",
+                prevEl: ".prev_1",
+            },
+        });
+    </script>
+
+</body>
+
+</html>
